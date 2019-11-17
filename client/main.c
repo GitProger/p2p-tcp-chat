36576d73843b03e89c1e744793fde3cc3561257e
GitProger <51924999+GitProger@users.noreply.github.com>
2019-11-17 21:23:45 +0300

    Create main.c

---
+++ client/main.c
@@ -0,0 +1,55 @@
+#include <winsock2.h>
+#include <windows.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <locale.h>
+#define DEFAULT_BUFLEN 65536
+#include "client.h"
+
+int main(int argc, char *argv[]) {
+    setlocale(LC_ALL, "Russian");
+    char ip[16], port[6] = "23", *msg, *buffer;
+    msg = malloc(DEFAULT_BUFLEN);
+    buffer = malloc(DEFAULT_BUFLEN);
+    if (!msg || !buffer) {
+        printf("No free memory.\n");
+        return 1;
+    }
+    if (argc >= 4) {
+        strcpy(ip, argv[1]);
+        strcpy(port, argv[2]);
+        strcpy(msg, argv[3]);
+    } else if (argc == 3) {
+        strcpy(ip, argv[1]);
+        strcpy(port, argv[2]);
+        printf("Message: ");        
+        gets_s(buffer, DEFAULT_BUFLEN);
+        sscanf(buffer, "%s", msg);
+    } else if (argc == 2) {
+        strcpy(ip, argv[1]);
+        printf("Port (default=23): ");
+        gets_s(buffer, 5);
+        sscanf(buffer, "%s", port);
+        printf("Message: ");        
+        gets_s(buffer, DEFAULT_BUFLEN);
+        sscanf(buffer, "%s", msg);
+    } else {
+        printf("IP address: ");
+        gets_s(buffer, 15);
+        sscanf(buffer, "%s", ip);
+        printf("Port (default=23): ");
+        gets_s(buffer, 5);
+        sscanf(buffer, "%s", port);
+        printf("Message: ");        
+        gets_s(buffer, DEFAULT_BUFLEN);
+        msg = buffer;
+    }
+    int res = client(ip, port, msg);
+    free(buffer);
+    free(msg);
+    return res;
+}
+
